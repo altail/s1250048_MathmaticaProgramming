eef584780b7cbd253daf142b331cfadbaf11907a
noritaka <33963908+keronori@users.noreply.github.com>
2018-04-23 14:27:27 +0900

    Add files via upload

---
+++ ホイヘンス14.java
@@ -0,0 +1,16 @@
+public class sample{
+  public static void main(String[] args){
+    double basic=35.0/36;
+    double now=35.0/36;
+    int n=1;
+
+    while(true){
+      if(now<0.1){
+        System.out.println(n);
+        break;
+      }
+      n++;
+      now*=basic;
+    }
+  }
+}
