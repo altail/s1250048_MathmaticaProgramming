9d0fd43696cb479ae69715aa7fc69813cb247d72
noritaka <33963908+keronori@users.noreply.github.com>
2018-04-23 22:14:53 +0900

    Add files via upload
    n人における誕生日が一緒の人が1組以上いる確率。

---
+++ sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int n;
+  double now=1;
+  double text=1;
+
+  printf("人数を入力してください: ");
+  scanf("%d",&n);
+
+  for(int i=1;i<n;i++){
+    now*=(365.0-i)/365.0;
+    text*=1-(i/365.0);
+  }
+
+  printf("%f",1-now);
+  printf("\n教科書ver.\n%f\n",1-text);
+}
